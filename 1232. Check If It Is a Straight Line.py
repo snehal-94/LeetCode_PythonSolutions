1c89ce0795e5dc3dd1972d017922974a56157d63
Snehal Sinha <snehal.sinha09@gmail.com>
2020-06-03 21:08:20 +0530

    Create 1232. Check If It Is a Straight Line.py

---
+++ 1232. Check If It Is a Straight Line.py
@@ -0,0 +1,46 @@
+'''
+You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+Example 1:
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
+
+Example 2:
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+ 
+
+Constraints:
+
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates contains no duplicate point.
+'''
+Solution:
+________________________________________________
+
+import math
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        if len(coordinates)==2:
+            return True
+        if(coordinates[1][0]==coordinates[0][0]):
+            slope=math.inf
+        else:
+            slope=(coordinates[1][1]-coordinates[0][1])/(coordinates[1][0]-coordinates[0][0])
+        i=1
+        for j in range(2,len(coordinates)):
+            x2=coordinates[i][0]
+            y2=coordinates[i][1]
+            x3=coordinates[j][0]
+            y3=coordinates[j][1]
+            if(x3==x2):
+                slopeA=math.inf
+            else:
+            # slopeA=(y2-y1)//(x2-x1)
+                slopeA=(y3-y2)/(x3-x2)
+            if (slopeA != slope):
+                return False
+            i+=1
+        return True
