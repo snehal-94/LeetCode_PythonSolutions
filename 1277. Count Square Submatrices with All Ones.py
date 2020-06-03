9fc3cdaddc515552696b63a5d727e6c7729db071
Snehal Sinha <snehal.sinha09@gmail.com>
2020-06-04 03:58:13 +0530

    Create 1277. Count Square Submatrices with All Ones.py

---
+++ 1277. Count Square Submatrices with All Ones.py
@@ -0,0 +1,54 @@
+Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+ 
+
+Example 1:
+
+Input: matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+Output: 15
+Explanation: 
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+Example 2:
+
+Input: matrix = 
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+]
+Output: 7
+Explanation: 
+There are 6 squares of side 1.  
+There is 1 square of side 2. 
+Total number of squares = 6 + 1 = 7.
+ 
+
+Constraints:
+
+1 <= arr.length <= 300
+1 <= arr[0].length <= 300
+0 <= arr[i][j] <= 1
+
+Solution
+________________________________________________
+
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        m=len(matrix)
+        n=len(matrix[0])
+        count=0
+        newMatrix=[[0]*(n+1) for _ in range(m+1)]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if matrix[i-1][j-1]==1:
+                    newMatrix[i][j]=1+min(newMatrix[i][j-1],newMatrix[i-1][j],newMatrix[i-1][j-1])
+                    count+=newMatrix[i][j]
+        return count
