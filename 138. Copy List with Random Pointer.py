c6b5257f03e86a6a3faec904208f64852bf69753
Snehal Sinha <snehal.sinha09@gmail.com>
2020-06-04 02:36:23 +0530

    Create 138. Copy List with Random Pointer.py

---
+++ 138. Copy List with Random Pointer.py
@@ -0,0 +1,69 @@
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+The Linked List is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not point to any node.
+ 
+
+Example 1:
+
+
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Example 2:
+
+
+Input: head = [[1,1],[2,1]]
+Output: [[1,1],[2,1]]
+Example 3:
+
+
+
+Input: head = [[3,null],[3,0],[3,null]]
+Output: [[3,null],[3,0],[3,null]]
+Example 4:
+
+Input: head = []
+Output: []
+Explanation: Given linked list is empty (null pointer), so return null.
+ 
+
+Constraints:
+
+-10000 <= Node.val <= 10000
+Node.random is null or pointing to a node in the linked list.
+Number of Nodes will not exceed 1000.
+
+
+Solution
+____________________________________________
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+import copy
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        # return copy.deepcopy(head)
+        dummy=curr=Node(0)
+        dict={}
+        while head:
+            if head not in dict:
+                dict[head]=Node(head.val)
+            curr.next=dict[head]
+            if head.random:
+                if head.random not in dict:
+                    dict[head.random]=Node(head.random.val)
+                curr.next.random=dict[head.random]
+            curr=curr.next
+            head=head.next
+        return dummy.next
+        
+        
