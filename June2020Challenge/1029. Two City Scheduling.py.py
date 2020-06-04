98f9d99dbef87d2703126351a540a3a8d39607c5
Snehal Sinha <snehal.sinha09@gmail.com>
2020-06-05 01:16:45 +0530

    Update and rename 1029. Two City Scheduling.py to June2020Challenge/1029. Two City Scheduling.py.py

--- June2020Challenge/1029. Two City Scheduling.py.py
+++ June2020Challenge/1029. Two City Scheduling.py.py
@@ -2,8 +2,6 @@ There are 2N people a company is planning to interview. The cost of flying the i
 
 Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
 
- 
-
 Example 1:
 
 Input: [[10,20],[30,200],[400,50],[30,20]]
