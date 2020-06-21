23dacd53cf7839fa48743188af00fe5eda75a92d
Snehal Sinha <snehal.sinha09@gmail.com>
2020-06-22 01:08:59 +0530

    Update Permutation Sequence.py

--- June2020Challenge/Permutation Sequence.py
+++ June2020Challenge/Permutation Sequence.py
@@ -25,6 +25,57 @@ Output: "2314"
 
 Solution_________________________________________________________________
 
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+My solution is basically the same with the many others but here is another explanation:
+
+Let's go over an example:
+
+n=4  k=9
+1234 ------ start here
+1243 ------ third digit changes here 
+1324 ------ second digit changes here 
+1342
+1423
+1432 
+2134 ------ first digit changes here 
+2143
+2314 -> k=9
+2341
+2413
+2431
+3124 ------ first digit changes here 
+.
+.
+.
+As you can see first digit changes after 6 occurances which is (n-1)! and the second digit changes after 2 occurances which is (n-2)!. 
+Similarly third digit changes after 1 occurances which is (n-3)!. 
+Is this a coincidance? Of course not. Since it is a permutation we compute it like this:
+(n)(n-1)(n-2)...(1) each paranthesis represents a digit. for the first place, we have n options. 
+After using one of the numbers, we cannot use it again. So we have n-1 number options for the second place. 
+In the end we multiply them to get the total number of permutations. Let's say we picked '1' for the first place. 
+now we have (n-1)! options for the rest of the number. This is why at each step a number is repeated that many time.
+
+Let's go back to our example:
+
+Since the first digit is repeated (n-1)! times, by dividing the k by n we can find our first digit. 
+Division must be integer division because we are only interested in the integer part.
+
+k=9, n=4
+(n-1)! = 6
+k /(n-1)!  = 1
+remainder = 3
+Numbers that we can use as digits are = [1,2,...,n]
+
+So, our first digit is the digit at index 1 which is 2. 
+We take the digit at 1 because our list is sorted so we are sure that the second smallest digit is at index 1.
+
+Since we used 2, we need to remove it from our list and k takes the value of the remainder. 
+You can think of lit ike this: we decided on our first digit so we can discard that part and deal with the rest of the number. 
+As you can see the problem is the same! but this time we have (n-2)! instead of (n-1)!,
+k=remainder and we have one less number in our list.
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         factor = factorial(n-1)
