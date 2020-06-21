d4cd59f6c2699c2978c157aef4306e88a41f0aa4
Snehal Sinha <snehal.sinha09@gmail.com>
2020-06-22 01:06:19 +0530

    Create   Permutation Sequence.py

---
+++ June2020Challenge/Permutation Sequence.py
@@ -0,0 +1,44 @@
+The set [1,2,3,...,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+Note:
+
+Given n will be between 1 and 9 inclusive.
+Given k will be between 1 and n! inclusive.
+Example 1:
+
+Input: n = 3, k = 3
+Output: "213"
+Example 2:
+
+Input: n = 4, k = 9
+Output: "2314"
+
+Solution_________________________________________________________________
+
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        factor = factorial(n-1)
+        k -= 1 # index starts from 1 in the question but our list indexs starts from 0
+        ans = []
+        numbers_left = list(range(1,n+1))
+        
+        for m in range(n-1,0,-1):
+            index = int(k // factor)
+            ans += str(numbers_left[index])
+            numbers_left.pop(index)
+            k %= factor
+            factor /= m
+            
+        ans += str(numbers_left[0])
+        return ''.join(ans)
+        
